8c25ec8ef087c3312bf00ffdf4bbed4d5274e5d0
DelaCruz-Angel <mobilen1cole58@gmail.com>
2025-09-09 09:54:13 +0800

    Added student variables

---
+++ DelaCruzLife.js/DelaCruzLife.js
@@ -0,0 +1,7 @@
+
+let name = "Angel Dela Cruz";
+let course = "Bachelor of Science in Information Technology";
+let YearLevel = "2nd year";
+
+
+console.log("Name: " +name + "\nCourse: " +course + "\nYear Level: " +YearLevel);
